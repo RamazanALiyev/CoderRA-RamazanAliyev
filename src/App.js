f1c04454237dd03f1699c5bc8efc126bbab8efd9
RamazanAliyev <aliyev.ramazan98@gmail.com>
2022-06-18 16:51:00 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import StudentSwiper from "./components/MainCoderra/StudentSwiper/StudentSwiper"
 import { Maincontext } from "./context";
 import { Routes, Route } from "react-router-dom";
 import Connect from "./pages/Connect/Connect";
-import Loading from './pages/Loading/Loading';
+import Loading from "./pages/Loading/Loading";
 import ToTop from "./components/MainCoderra/ScrollToTop/ToTop";
 import Sidebar from "./pages/Header/Sidebar/Sidebar";
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [apply, setApply] = useState(false);
   const [forCourseApply, setForCourseApply] = useState(false);
   setTimeout(() => {
@@ -43,7 +43,7 @@ function App() {
             <Route path="/studentswiper" element={<StudentSwiper />} />
             <Route path="/connect" element={<Connect />} />
           </Routes>
-          <ToTop/>
+          <ToTop />
           <Footer />
         </div>
       )}
